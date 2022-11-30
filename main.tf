f3cea6206ec1fd371047fe0e63ae0963409c8ec2
Erik-Kostelansky <119487037+Erik-Kostelansky@users.noreply.github.com>
2022-11-30 18:20:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,19 +62,25 @@ resource "aws_instance" "web" {
               #!/bin/bash
               apt-get update
               apt-get install nginx
-              sed -i -e 's/80/8080/' /etc/nginx/sites-enabled/default
-              systemctl restart nginx
               EOF
 }
 
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
   egress {
     from_port   = 0
@@ -85,7 +91,7 @@ resource "aws_security_group" "web-sg" {
 }
 
 output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+  value = "${aws_instance.web.public_dns}"
 }
 
 output "private_key" {
