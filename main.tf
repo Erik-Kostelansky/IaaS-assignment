0080252b7731118bc22c341b9a9a12c790a1f6a6
Erik Kostelansky <kostelansky@ejoin.eu>
2022-12-02 10:23:07 +0100

    Remove load balancer reference in asg

--- main.tf
+++ main.tf
@@ -51,7 +51,6 @@ resource "aws_autoscaling_group" "ubuntu_autoscaling_group" {
   min_size            = 1
   desired_capacity    = 2
   vpc_zone_identifier = [aws_default_subnet.default_subnet.id]
-  load_balancers      = [aws_lb.lb.id]
   target_group_arns   = [aws_lb_target_group.lb_tg.arn]
 
   launch_template {
