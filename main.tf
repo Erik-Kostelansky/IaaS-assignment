227f295bd6b67f3a5fca24994733f9f0104b9238
Erik Kostelansky <kostelansky@ejoin.eu>
2022-12-02 10:04:56 +0100

    Pridanie schedule pre scaling group

--- main.tf
+++ main.tf
@@ -24,10 +24,21 @@ resource "aws_default_subnet" "default_subnet" {
   availability_zone = "eu-central-1a"
 }
 
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "testKey"
+  public_key = tls_private_key.example.public_key_openssh
+}
+
 resource "aws_launch_template" "ubuntu_launch_template" {
   name_prefix   = "ubuntu_launch_template"
   image_id      = "ami-0caef02b518350c8b"
   instance_type = "t2.micro"
+  key_name      = aws_key_pair.generated_key.key_name
 
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
@@ -36,21 +47,31 @@ resource "aws_launch_template" "ubuntu_launch_template" {
 
 resource "aws_autoscaling_group" "ubuntu_autoscaling_group" {
   name                = "ubuntu_autoscaling_group"
-  desired_capacity    = 2
   max_size            = 2
   min_size            = 1
+  desired_capacity    = 2
   vpc_zone_identifier = [aws_default_subnet.default_subnet.id]
+  load_balancers      = [aws_lb.lb.id]
 
   launch_template {
     id = aws_launch_template.ubuntu_launch_template.id
   }
 }
 
+resource "aws_autoscaling_schedule" "scale_down_group_at" {
+  scheduled_action_name  = "scale_down_group_at"
+  desired_capacity       = 1
+  start_time             = "2022-12-02T17:00:00Z"
+  end_time               = "2022-12-03T07:00:00Z"
+  recurrence             = "0 0 * * 1-5"
+  autoscaling_group_name = aws_autoscaling_group.ubuntu_autoscaling_group.name
+}
+
 resource "aws_security_group" "web-sg" {
   name = "web-sg"
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 31555
+    to_port     = 31555
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -88,7 +109,7 @@ resource "aws_lb_listener" "lg_listener" {
 }
 
 
-# output "web-address" {
-#   value = "${aws_instance.web.public_dns}"
-# }
+output "load-balancer-address" {
+  value = aws_lb.aws_lb.dns_name
+}
 
--- userDataScript.sh
+++ userDataScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
 apt-get install -y apache2
-sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-echo "Hello World" > /var/www/html/index.html
+sed -i -e 's/80/31555/' /etc/apache2/ports.conf
+echo "DevOps mentor assignment 4 is functional" > /var/www/html/index.html
 systemctl restart apache2
